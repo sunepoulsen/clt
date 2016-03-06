fe725474409aaf6b0661e03e40a2706c15bd3397
Sune Thomas Poulsen <sunepoulsen@gmail.com>
2016-03-06 22:27:02 +0100

    First api definition.

---
+++ src/main/java/dk/sunepoulsen/clt/api/SubcommandExecutor.java
@@ -0,0 +1,27 @@
+//-----------------------------------------------------------------------------
+package dk.sunepoulsen.clt.api;
+
+//-----------------------------------------------------------------------------
+/**
+ * Interface for an executor to execute the logic of a parsed sub command.
+ * <p>
+ *     Each sub command should have its own sub command executor to execute
+ *     the sub command.
+ * </p>
+ * <p>
+ *     Implementation classes will hold attributes for each argument or option
+ *     that is supported by the sub command on the command line. <code>validateArguments()</code>
+ *     is called to validate the values of the arguments. For instance to check if a file exists.
+ * </p>
+ */
+public interface SubCommandExecutor {
+    /**
+     * Validates the state of arguments to the sub command.
+     */
+    void validateArguments();
+
+    /**
+     * Performs the action of a sub command.
+     */
+    void performAction();
+}
